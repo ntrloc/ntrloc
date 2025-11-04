fe99a3ed8cd8e91d4290e6d17414e8fa82f260d7
michaelcoddington <mcoddington@scholastic.com>
2025-11-04 09:39:01 -0500

    Sonar fix

--- domain-graph-starter/src/main/java/org/ntrloc/graph/graphql/scalars/DateScalar.java
+++ domain-graph-starter/src/main/java/org/ntrloc/graph/graphql/scalars/DateScalar.java
@@ -19,14 +19,15 @@ import java.util.Locale;
 @DgsScalar(name = "Date")
 public class DateScalar implements Coercing<String, String> {
 
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private static final String DATE_ERROR_MESSAGE = "Not a valid date";
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Override
     public @Nullable String serialize(@NonNull Object dataFetcherResult, @NonNull GraphQLContext graphQLContext, @NonNull Locale locale) throws CoercingSerializeException {
         if (dataFetcherResult instanceof String d) {
             try {
-                DATE_FORMAT.parse(d);
+                dateFormat.parse(d);
                 return d;
             } catch (ParseException pe) {
                 throw new CoercingSerializeException(DATE_ERROR_MESSAGE);
@@ -39,7 +40,7 @@ public class DateScalar implements Coercing<String, String> {
     public @Nullable String parseValue(@NonNull Object input, @NonNull GraphQLContext graphQLContext, @NonNull Locale locale) throws CoercingParseValueException {
         if (input instanceof String s) {
             try {
-                DATE_FORMAT.parse(s);
+                dateFormat.parse(s);
                 return s;
             } catch (ParseException pe) {
                 throw new CoercingSerializeException(DATE_ERROR_MESSAGE);
@@ -52,7 +53,7 @@ public class DateScalar implements Coercing<String, String> {
     public @Nullable String parseLiteral(@NonNull Value<?> input, @NonNull CoercedVariables variables, @NonNull GraphQLContext graphQLContext, @NonNull Locale locale) throws CoercingParseLiteralException {
         if (input instanceof StringValue sv) {
             try {
-                DATE_FORMAT.parse(sv.getValue());
+                dateFormat.parse(sv.getValue());
                 return sv.getValue();
             } catch (ParseException pe) {
                 throw new CoercingSerializeException(DATE_ERROR_MESSAGE);
